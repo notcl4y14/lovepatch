9896740a50d47b715a2c158f98163c2ef1963153
notcl4y14 <128468209+notcl4y14@users.noreply.github.com>
2023-06-30 14:30:27 +0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,24 @@
+local lovepatch = require("lovepatch")
+
+function love.load()
+	love.graphics.setDefaultFilter("nearest")
+	img = lovepatch.load("img.png", 10, 10)
+
+	x = 10
+	y = 10
+	width = 10
+	height = 10
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+	love.graphics.clear(0.9, 0.9, 0.9, 1)
+	lovepatch.draw(img, x, y, width, height)
+end
+
+function love.mousemoved(mouseX, mouseY)
+	width = x + mouseX
+	height = y + mouseY
+end
